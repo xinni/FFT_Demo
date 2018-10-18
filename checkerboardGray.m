d168492baa5350e1b614b55af992fc368e5c46bd
xinni <xinni@buffalo.edu>
2018-10-18 14:53:49 -0400

    clean up whole project

--- checkerboardGray.m
+++
@@ -1,9 +0,0 @@
-numsquares = [64, 64]; %rows columns
-squaresize = 1;
-pattern = rand([numsquares, 1]);
-checkerboard = imresize(pattern, squaresize, 'nearest');
-imshow(checkerboard);
-filename='randBoard.jpg';
-% saveas(gcf,filename);
-K = medfilt2(checkerboard);
-figure, imshow(K);
